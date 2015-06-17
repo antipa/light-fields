55dbda3fb691ae592efc2f79380dccdfc52462cc
Sylvia Necula <sylvia.necula@gmail.com>
2015-06-17 13:38:30 -0700

    Creates A matrix for any grid size and grid range.

--- check.m
+++ check.m
@@ -1,55 +1,42 @@
 backwardRayTracing;
 
-figure(1);
-hold on
-grid on
-aMatrix = zeros(sensorSizeX,9);
-for i = 1:sensorSizeX
-    for j = 1:9
-        list = find(output{2,1} == i);
-        scatter(output{2,2}(list),output{2,3}(list));
-        count = 0;
-        for k = 1:length(list)
-            switch j
-                case 1
-                    if output{2,2}(list(k)) > 0 && output{2,2}(list(k)) < 33.33 && output{2,3}(list(k)) > 1.333 && output{2,3}(list(k)) < 4
-                        count = count + 1;
-                    end
-                case 2
-                    if output{2,2}(list(k)) > 0 && output{2,2}(list(k)) < 33.33 && output{2,3}(list(k)) > -1.333 && output{2,3}(list(k)) < 1.33
-                        count = count + 1;
-                    end
-                case 3
-                    if output{2,2}(list(k)) > 0 && output{2,2}(list(k)) < 33.33 && output{2,3}(list(k)) > -4 && output{2,3}(list(k)) < -1.33
-                        count = count + 1;
-                    end
-                case 4
-                    if output{2,2}(list(k)) > 33.33 && output{2,2}(list(k)) < 66.66 && output{2,3}(list(k)) > 1.333 && output{2,3}(list(k)) < 4
-                        count = count + 1;
-                    end
-                case 5
-                    if output{2,2}(list(k)) > 33.33 && output{2,2}(list(k)) < 66.66 && output{2,3}(list(k)) > -1.333 && output{2,3}(list(k)) < 1.33
-                        count = count + 1;
-                    end
-                case 6
-                    if output{2,2}(list(k)) > 33.33 && output{2,2}(list(k)) < 66.66 && output{2,3}(list(k)) > -4 && output{2,3}(list(k)) < -1.33
-                        count = count + 1;
-                    end
-                case 7
-                    if output{2,2}(list(k)) > 66.66 && output{2,2}(list(k)) < 99.99 && output{2,3}(list(k)) > 1.333 && output{2,3}(list(k)) < 4
-                        count = count + 1;
-                    end
-                case 8
-                    if output{2,2}(list(k)) > 66.66 && output{2,2}(list(k)) < 99.99 && output{2,3}(list(k)) > -1.333 && output{2,3}(list(k)) < 1.33
-                        count = count + 1;
-                    end
-                case 9
-                    if output{2,2}(list(k)) > 66.66 && output{2,2}(list(k)) < 99.99 && output{2,3}(list(k)) > -4 && output{2,3}(list(k)) < -1.33
-                        count = count + 1;
-                    end
-            end
-        end
-        aMatrix(i,j) = count ./ raysPerPixel;
+gridX = 4;
+gridT = 4;
+xRange = [0 100];
+tRange = [-4 4];
+stepX = (max(xRange) - min(xRange)) ./ gridX;
+stepT = (max(tRange) - min(tRange)) ./ gridT;
+xValues = [];
+for l = xRange(1):stepX:xRange(2)
+    for q = 1:gridX
+        xValues = [xValues l];
     end
 end
-hold off
+yValues = [];
+for l = tRange(2):-stepT:tRange(1)
+    for q = 1:gridT
+        yValues = [yValues l];
+    end
+end
+    figure(1);
+    hold on
+    grid on
+    aMatrix = zeros(sensorSizeX, gridX .* gridT);
+    for i = 1:sensorSizeX
+        for j = 1: gridX * gridT
+            xmin = xValues(j);
+            xmax = xValues(j+gridX);
+            ymax = yValues(j);
+            ymin = yValues(j+gridT);
+            list = find(output{2,1} == i);
+            scatter(output{2,2}(list),output{2,3}(list));
+            count = 0;
+            for k = 1:length(list)
+                if output{2,2}(list(k)) > xmin && output{2,2}(list(k)) < xmax && output{2,3}(list(k)) > ymin && output{2,3}(list(k)) < ymax
+                    count = count + 1;
+                end
+            end
+            aMatrix(i,j) = count ./ raysPerPixel;
+        end
+    end
+    hold off
