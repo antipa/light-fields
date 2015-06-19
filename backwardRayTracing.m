1420d831cc25e629c1b1747a3f16d0ff29d66e69
Sylvia Necula <sylvia.necula@gmail.com>
2015-06-19 14:49:50 -0700

    Added comments to backward ray tracing.

--- backwardRayTracing.m
+++ backwardRayTracing.m
@@ -30,6 +30,7 @@ if twoD
         currentPixel = currentPixel + 1;
     end
 else
+    %pixel indexing goes from bottom to top, left to right
     for i = 0:sensorSizeX - 1
         for j = 0:sensorSizeY - 1
             xr = [xr; (rand(raysPerPixel,1) + i)*pixelSize];
@@ -69,7 +70,7 @@ diffuser_in = in.filtered * strengthB;
 
 %coordinate system in physical units from the diffuser file
 x = in.x;
-%first pixel starts at 0
+%first pixel starts at 0 and goes to 6000
 x = x - min(x);
 diff_upsample = false;
 if diff_upsample
@@ -84,38 +85,13 @@ px = mean(diff(x)); %diffuser "pixel" size in um/pixel
 
 
 if twoD
-    %Setup gradients
-    % if dy_idx == 0 %flat in the y-direction
+    %setup gradients
+    %using first row of the diffuser
     Fx = gradient(diffuser(1,:));
     Fy = zeros(size(Fx));
-    % elseif dx_idx == 0 %flat in the x-direction
-    %     Fy = gradient(diffuser);
-    %     Fx = zeros(size(Fy));
-    % else %sloped in both directions
-    %     [Fx, Fy] = gradient(diffuser);
-    %  end
-    
-    %Calculate surface norm at random positions by
-    %interpolation within gradient
-    %--------------------------------------------------------------
-    % %Get z and surface normal at each random (xr(i),yr(i)) pair
-    % if dy_idx==0
-    %
-    %     %zr = interp1(xg,diff_crop,xr);   %Interpolate surface
-    % elseif dx_idx==0
-    %     Fyr = interp1(yg,Fy_crop,yr);  %Interpolate x gradient
-    %     Fxr = zeros(size(Fyr));
-    %     %zr = interp1(yg,diff_crop,yr);   %Interpolate surface
-    % else
-    %     %zr = interp2(xg,yg,diff_crop,xr,yr);   %Interpolate surface
-    %     Fyr = interp2(xg,yg,Fx_crop',xr,yr);  %Interpolate x gradient
-    %     Fxr = interp2(xg,yg,Fy_crop',xr,yr);  %Interpolate y gradiet
-    % end
-    %-----------------------------------------------------------
 else
     [Fx, Fy] = gradient(diffuser);
 end
-
 
 if twoD
     Fxr = interp1(x,Fx,xo); %Interpolate x gradient
@@ -125,6 +101,8 @@ else
     Fxr = interp2(x,y,Fy,xo,yo);
 end
 
+%throwing out points that did not hit the diffuser and could
+%not be interpolated
 good = ~isnan(Fxr) & ~isnan(Fyr);
 Fxr = Fxr(good);
 Fyr = Fyr(good);
@@ -133,7 +111,6 @@ ph = ph(good);
 xo = xo(good);
 yo = yo(good);
 pixelIndex = pixelIndex(good);
-
 
 %refraction
 [uxp, uyp,~] = refraction(Fxr, Fyr, th, ph, indexEnv, indexDiff);
@@ -154,6 +131,8 @@ output{2,4} = yo;
 output{1,5} = 'phi';
 output{2,5} = uyp;
 
+%constructing A matrix by dividing the output matrix cell array
+%into a grid
 stepX = (max(xRange) - min(xRange)) ./ gridX;
 stepT = (tRange(2) - tRange(1)) ./ gridT;
 xValues = [];
@@ -162,25 +141,33 @@ for l = xRange(1):stepX:xRange(2)
         xValues = [xValues l];
     end
 end
+
+%start with the bottom left corner
 yValues = [tRange(2):-stepT:tRange(1)];
 
 figure(1);
 hold on
 grid on
-aMatrix = zeros(sensorSizeX, gridX .* gridT);
+aMatrix = zeros(sensorSizeX, (gridX .* gridT));
 for i = 1:sensorSizeX
+    %indices of output from pixel i
     list = find(output{2,1} == i);
-%     scatter(output{2,2}(list),output{2,3}(list) * indexDiff);
+   scatter(output{2,2}(list),output{2,3}(list) * indexDiff);
     for j = 1: gridX * gridT
         xmin = xValues(j);
         xmax = xValues(j+gridX);
         ymin = yValues(mod(j-1,length(yValues)-1) + 1);
         ymax = yValues(mod(j-1,length(yValues)-1) + 2);
-        count = 0;
+        
+        %logical to find rays from pixel i that are in the grid square
+        %you are considering
         a = output{2,2}(list) > xmin & output{2,2}(list) <= xmax & ...
             output{2,3}(list) > ymin & output{2,3}(list) <= ymax;
+        %logical to find rays from pixel i that hit the grid
         b = output{2,2}(list) > xRange(1) & output{2,2}(list) <= xRange(2) & ...
             output{2,3}(list) > tRange(2) & output{2,3}(list) <= tRange(1);
+        %if no rays from pixel i hit the grid,record normalized number of 
+        %rays as 0
         if sum(b)
         aMatrix(i,j) = sum(a) ./ sum(b);
         end
