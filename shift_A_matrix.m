2956fdb91fa6bdc1a063f218f6a87133a04fa8c2
Nick Antipa <naantipa@gmail.com>
2015-07-02 17:38:42 -0700

    Fixed to shift_A_matrix to work with sub pixel shifts. Also corrected a few other issue with incorrect functioning using npx~=npy.

--- shift_A_matrix.m
+++ shift_A_matrix.m
@@ -1,4 +1,4 @@
-function [A_shifted, shifter]= shift_A_matrix(A_sub1,dx,dy,npx,npy)
+function A_shifted = shift_A_matrix(A_sub1,dx,dy,npx,npy)
 %A_shifted = shift_A_matrix(A_sub1,dx,dy,npx,npy)
 %
 %Shift A matrix projection by dx,dy (positive dx shifts left, positive dy
@@ -6,33 +6,56 @@ function [A_shifted, shifter]= shift_A_matrix(A_sub1,dx,dy,npx,npy)
 dx_px = floor(dx);
 dy_px = floor(dy);
 
-cstart = max(dx_px*npy+1+dy_px,1);
-rstart = max(-dx_px*npy+1-dy_px,1);
-cstartL = floor(cstart);
-rstartL = floor(rstart);
-b = mod(rstartL-1:npx*npy-cstartL,npx)<(npy-dy_px);
-a = mod(rstartL-1:npx*npy-cstartL,npx)>-dy_px;
-spvalsL = a.*b;
-shifter = sparse(cstartL:npx*npy-rstartL+1,rstartL:npx*npy-cstartL+1,spvalsL,npx*npy,npx*npy);
-%shifter = sparse(rstart:npx*npy-cstart+1,cstart:npx*npy-rstart+1,spvals,npx*npy,npx*npy);
 
-%A is now shifted by dx_px and dy_px. Still need to check for and apply
-%sub-pixel shifts if necessary.
+
+
 
 residual_x = abs(dx-dx_px);
 residual_y = abs(dy-dy_px);
-if residual_x
-    [~, shifter_x] = shift_A_matrix(A_sub1,dx_px+1,dy_px,npx,npy);
-    shifter = shifter*(1-residual_x)+residual_x*shifter_x;
+
+if ~residual_x && ~residual_y
+    shifter = make_shifter(dx_px,dy_px,npx,npy);
     A_shifted = shifter*A_sub1;
+    return
+elseif residual_x && ~residual_y
+    %Only x is subpixel
+    shifter_x1 = make_shifter(dx_px,dy_px,npx,npy);
+    shifter_x2 = make_shifter(dx_px+1,dy_px,npx,npy);
+    shifter = shifter_x1*(1-residual_x) + shifter_x2*residual_x;
+    A_shifted = shifter*A_sub1;
+    return
+elseif ~residual_x && residual_y
+    %Only y is subpixel
+    shifter_y1 = make_shifter(dx_px,dy_px,npx,npy);
+    shifter_y2 = make_shifter(dx_px,dy_px+1,npx,npy);
+    shifter = shifter_y1*(1-residual_y) + shifter_y2*residual_y;
+    A_shifted = shifter*A_sub1;
+    return
+elseif residual_x && residual_y
+    %Both are subpixel
+    %Shift X first
+    shifter_x1 = make_shifter(dx_px,0,npx,npy);
+    shifter_x2 = make_shifter(dx_px+1,0,npx,npy);
+    shifter_x = shifter_x1*(1-residual_x) + shifter_x2*residual_x;
+    A_shiftedx = shifter_x*A_sub1;
+    shifter_y1 = make_shifter(0,dy_px,npx,npy);
+    shifter_y2 = make_shifter(0,dy_px+1,npx,npy);
+    shifter_y = shifter_y1*(1-residual_y) + shifter_y2*residual_y;
+    A_shifted = shifter_y*A_shiftedx;
+    return
 end
 
-if residual_y
-    [~, shifter_y] = shift_A_matrix(A_sub1,dx,dy_px+1,npx,npy);
-    shifter = shifter*(1-residual_y)+residual_y*shifter_y;
 end
 
-A_shifted = shifter*A_sub1;
+function shifter = make_shifter(dx_px,dy_px,npx,npy)
 
+    cstart = max(dx_px*npx+1+dy_px,1);
+    rstart = max(-dx_px*npx+1-dy_px,1);
 
-return
+    b = mod(rstart-1:npx*npy-cstart,npx)<(npx-dy_px);
+    a = mod(rstart-1:npx*npy-cstart,npx)>=-dy_px;
+    spvals = a.*b;
+    shifter = sparse(cstart:npx*npy-rstart+1,rstart:npx*npy-cstart+1,spvals,npx*npy,npx*npy);
+    
+    
+end
