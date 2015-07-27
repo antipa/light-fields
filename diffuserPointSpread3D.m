1169ffe27ef4aa54958e5637fb807ac00a222a12
Sylvia Necula <sylvia.necula@gmail.com>
2015-07-27 12:57:17 -0700

    Found indexing error when constructing the H matrix.

--- diffuserPointSpread3D.m
+++ diffuserPointSpread3D.m
@@ -1,33 +1,40 @@
-%voxel dimensions
-vx = 2; %in x-direction
-vy = 4; %in y-direction
-vz = 6; %in z-direction
+%object dimensions in voxels
+vx = 10; %in x-direction
+vy = 10; %in y-direction
+vz = 10; %in z-direction
 
-%total rays traced from all z-planes of voxel
-rays = 1000;
+%voxel dimensions in microns
+voxX = 10;
+voxY = 10;
+voxZ = 100;
+
+%total rays traced from entire object
+rays = 10000000;
 
 %angle spread of rays, in degrees
 thetaSpread = 2;
 phiSpread = 2;
 
-%define number of planes in z-direction
-k = 5;
-dz = vz/k;
+%define number of planes in z-direction for each voxel
+k = 1;
 
-%distance from closest plane of voxel to diffuser
-z0 = 100;
+%assuming voxels are size 1
+dz = voxZ/k;
 
-%distance from diffuser to sensor
-z1 = 150;
+%distance from closest plane of voxel to diffuser, in microns
+z0 = 0;
+
+%distance from diffuser to sensor, in microns
+z1 = 200;
 
 %sensor dimensions
-npx = 5; %number of pixels in x-direction
-npy = 5; %number of pixels in y-direction
-x_range = 5; %total range of sensor in x-direction, in microns
-y_range = 5; %total range of sensor in y-direction, in microns
+npx = 50; %number of pixels in x-direction
+npy = 50; %number of pixels in y-direction
+x_range = 150; %total range of sensor in x-direction, in microns
+y_range = 150; %total range of sensor in y-direction, in microns
 
-%rays traced from each plane
-raysPerPixel = round(rays./k);
+%rays traced from each voxel
+raysPerVoxel = round(rays./(vx*vy*vz));
 
 %diffuser properties
 strength = 50;
@@ -56,50 +63,54 @@ px = mean(diff(x)); %diffuser "pixel" size in um/pixel, physical units
 [Fx, Fy] = gradient(diffuser);
 
 %initialize the histogram
-gatherer = zeros(npy,npx);
+hMatrix = zeros(npx*npy,vx*vy*vz);
 
-%repeat for each plane in z-direction
-for j = 1:k
-    
-%random x, y, theta, and phi column vectors   
-xr = rand(raysPerPixel,1) * vx;
-yr = rand(raysPerPixel,1) * vy;
-th = (rand(raysPerPixel,1) - 0.5) * thetaSpread;
-ph = (rand(raysPerPixel,1) - 0.5) * phiSpread;
- 
-%distance from z-plane to diffuser
-z = z0 + dz * (j-1);
-
-%propagate rays to the diffuser
-xo = z * tand(th) + xr;
-yo = z * tand(ph) + yr;
-
-Fyr = interp2(x,y,Fy,xo,yo);
-Fxr = interp2(x,y,Fx,xo,yo);
-
-%throwing out points that did not hit the diffuser and could
-%not be interpolated
-good = ~isnan(Fxr) & ~isnan(Fyr);
-Fxr = Fxr(good);
-Fyr = Fyr(good);
-th = th(good);
-ph = ph(good);
-xo = xo(good);
-yo = yo(good);
-            
-%outputs direction cosines after refraction
-[uxp, uyp, uzp] = refraction(Fxr, Fyr, th, ph, indexDiff, indexEnv,'angles');
-
-%propagate from diffuser to the sensor
-[yo, xo] = propagation(uyp, uzp, z1, yo, uxp, xo);
-
-%create image at the sensor using 2D histogramming
-dpx = x_range/npx;
-dpy = y_range/npy;
-% clf
-% figure(1)
-% imagesc(gather_rays_nohist(xo,yo,npx,npy,dpx,dpy,0,0,px));
-gatherer = gatherer + gather_rays_nohist(xo,yo,npx,npy,dpx,dpy,0,0,px);
+for a = 1:vx
+    for b = 1:vy
+        %repeat for each plane in z-direction
+        for c = 1:vz
+            gatherer = zeros(npy,npx);
+            for j = 1:k
+                %random x, y, theta, and phi column vectors
+                xr = (rand(raysPerVoxel,1) + (a-1)) * voxX;
+                yr = (rand(raysPerVoxel,1) + (b-1)) * voxY;
+                th = (rand(raysPerVoxel,1) - 0.5) * thetaSpread;
+                ph = (rand(raysPerVoxel,1) - 0.5) * phiSpread;
+                
+                %distance from z-plane to diffuser
+                z = z0 + voxZ * (c-1) + dz * (j-1);
+                
+                %propagate rays to the diffuser
+                xo = z * tand(th) + xr;
+                yo = z * tand(ph) + yr;
+                
+                Fyr = interp2(x,y,Fy,xo,yo);
+                Fxr = interp2(x,y,Fx,xo,yo);
+                
+                %throwing out points that did not hit the diffuser and could
+                %not be interpolated
+                good = ~isnan(Fxr) & ~isnan(Fyr);
+                Fxr = Fxr(good);
+                Fyr = Fyr(good);
+                th = th(good);
+                ph = ph(good);
+                xo = xo(good);
+                yo = yo(good);
+                
+                %outputs direction cosines after refraction
+                [uxp, uyp, uzp] = refraction(Fxr, Fyr, th, ph, indexDiff, indexEnv,'angles');
+                
+                %propagate from diffuser to the sensor
+                [yo, xo] = propagation(uyp, uzp, z1, yo, uxp, xo);
+                
+                %create image at the sensor using 2D histogramming
+                dpx = x_range/npx;
+                dpy = y_range/npy;
+                gatherer = gatherer + gather_rays_nohist(xo,yo,npx,npy,dpx,dpy,0,0,px);
+%                 imagesc(gatherer);
+%                 pause(1/24);
+            end
+            hMatrix(:,(sub2ind([vy,vx,vz],b,a,c))) = gatherer(:);
+        end
+    end
 end
-
-imagesc(gatherer);
